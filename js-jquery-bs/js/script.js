bf03670055b1080859717bf5327c5d70adef8698
CHACONI-PC\Usuario <ofab1307@gmail.com>
2017-04-04 17:46:47 -0600

    Lab 2 Oscar Fabián Chacón Bolaños

--- js-jquery-bs/js/script.js
+++ js-jquery-bs/js/script.js
@@ -1,88 +1,94 @@
 //Ejercicio 1
 
-muestra = function (texto, estilo) {
-  var print = $("#mostrarResultados");
-  if (print) {
-    print.attr("class", estilo);
-    print.text(texto);
+/*Funcion con el fin de mostrar resultados de consultas en el elemento HTML textarea*/
+mostrarResultados = function (texto, estilo) {
+  var x = $("#mostrarResultados");
+  if (x) {
+    x.attr("class", estilo);
+    x.text(texto);
   }
 }
 
+/*Funcion Constructora Usuario*/
 function Usuario(nombre, fecha) {
 
   //Propiedad privada
   var edad;
 
-  //Métodos privados
-    var calcularEdad = function() {
-    var fecha_actual = new Date().getYear();
-    var fecha_nacimiento = fecha.getYear();
+  //Método privado para calcular la edad.
+  var calcularEdad = function() {
+    var fecha_actual = new Date().getFullYear();
+    fecha_actual = parseInt(fecha_actual);
+    var fecha_nacimiento = fecha;
+    fecha_nacimiento = parseInt(fecha_nacimiento);
 
-    if (fecha_actual <= fecha_nacimiento)
-    edad = "Error: no se ha podido calcular";
-    else
-    edad = fecha_actual - fecha_nacimiento;
+    if (fecha_actual <= fecha_nacimiento) {
+      edad = "Error: no se ha podido calcular";
+    } else {
+      edad = (fecha_actual - fecha_nacimiento);
+    }
   };
 
   //Propiedades Públicas
   this._nombre = nombre;
   this._fechaN = fecha;
-  this._edad = edad;
-}
-//Método público presentarse
-presentarse = function() {
-  calcularEdad();
 
-  var resultado =
-    "Hola, mi nombre es " + this._nombre +
-    " y tengo " + this._edad + " años."
-    muestra(resultado, 'enviaTextArea');
-};
+  //Método público presentarse
+  this.presentarse = function() {
+    calcularEdad();
 
-mostrarResultados = function (texto, estilo) {
-  var print = $("#resultado");
-  if (print) {
-    output.attr("class", estilo);
-    output.text(texto);
-  }
+    if (edad != 0 && !isNaN(edad) && this._nombre.length  > 0) {
+            var resultado =
+                "Hola, mi nombre es " + this._nombre +
+                " y tengo " + edad + " años."
+            mostrarResultados(resultado, 'enviaTextArea');
+        } else {
+            mostrarResultados("Error, digite sus datos de manera correcta.", 'error');
+        }
+  };
 }
 
-// Ejercicio 2
+/* Ejercicio 2
+Aqui vamos a tener la funcionalidad de los botones.
+Donde segun el boton y la clase del css
+va a cambiar de color y texto como el ejercicio lo requiere.
+*/
+
 $("#button_calcularEdad").click(function () {
   var nombre = $("#nombre").val();
-  var fechaN = $("#fecha").val();
-  var rslt = parseInt(this._edad.substring(0, 4));
+  var fechaNac = $("#fecha").val();
+  var rslt = parseInt(fechaNac.substring(0, 4));
   var nuevoUsuario = new Usuario(nombre, rslt);
   nuevoUsuario.presentarse();
 });
 
 $("#button_idSelector").click(function () {
-  var text = $("#thing1").text();
-  mostrarResultados(text, "id-selector");
+  var txt = $("#thing1").text();
+  mostrarResultados(txt, "id-selector");
 });
 
 $("#button_claseSelector").click(function () {
-  var text = $(".special").text();
-  mostrarResultados(text, "class-selector");
+  var txt = $(".special").text();
+  mostrarResultados(txt, "class-selector");
 });
 
 $("#button_tagSelector").click(function () {
-  var text = $("li[data-troy='true']").text();
-  mostrarResultados(text, "tag-selector");
+  var txt = $("li[data-troy='true']").text();
+  mostrarResultados(txt, "tag-selector");
 });
 
 $("#button_selectorEditarTexto").click(function () {
-  var firstSelect = $("li[data-troy='true']").text();
-  var secondSelect = $("li[data-employee='Bob Metz']").text();
-  $("li[data-troy='true']").text(secondSelect);
-  $("li[data-employee='Bob Metz']").text(firstSelect);
+  var selectDataTroy = $("li[data-troy='true']").text();
+  var selectDataEmp = $("li[data-employee='Bob Metz']").text();
+  $("li[data-troy='true']").text(selectDataEmp);
+  $("li[data-employee='Bob Metz']").text(selectDataTroy);
   $("li[data-troy='true']").attr("class", "selector-edit-text");
   $("li[data-employee='Bob Metz']").attr("class", "selector-edit-text");
-  var result = firstSelect + " and " + secondSelect;
+  var result = selectDataTroy + " and " + selectDataEmp;
   mostrarResultados(result, "selector-editar-texto");
 });
 
 $("#button_selectorHijo").click(function () {
-  var text = $("div p").text();
-  mostrarResultados(text, "selector-hijo");
+  var txt = $("div p").text();
+  mostrarResultados(txt, "selector-hijo");
 });
